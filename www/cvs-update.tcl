000de3fd7b1c3968e2076f1483c88b10e1a93c09
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-12-08 13:27:43 +0000

    - modified ASUS to work with empty anonymous passwords

--- www/cvs-update.tcl
+++ www/cvs-update.tcl
@@ -131,9 +131,10 @@ if {"" != $cvs_password} {
     set cvs_password_phrase ":$cvs_password"
 }
 
+
 if {[catch {
 
-    set cmd "export HOME=$acs_root_dir; cvs -d :$cvs_protocol:$cvs_user$cvs_password_phrase@$cvs_server:$cvs_root login 2>&1"
+    set cmd "export HOME=$acs_root_dir; cvs -d :$cvs_protocol:$cvs_user:$cvs_password\@$cvs_server:$cvs_root login 2>&1"
     ns_write "$cmd\n"
 
     set fp [open "|[im_bash_command] -c \"$cmd\"" "w"]
@@ -161,7 +162,7 @@ ns_write "</pre>\n"
 # Execute the CVS command.
 # Example:
 #
-# cvs -z3 -d :pserver:anonymous@openacs.org:/cvsroot checkout -r oacs-4-6
+# cvs -z3 -d :pserver:anonymous\@openacs.org:/cvsroot checkout -r oacs-4-6
 
 if {!$error} {
     ns_write "<h2>CVS Update</h2>\n"
@@ -171,7 +172,7 @@ if {!$error} {
 
     if { [catch {
 
-	set cmd "export HOME=$acs_root_dir; cd $package_dir; cvs -z3 -d :$cvs_protocol:$cvs_user$cvs_password_phrase@$cvs_server:$cvs_root $cvs_command 2>&1"
+	set cmd "export HOME=$acs_root_dir; cd $package_dir; cvs -z3 -d :$cvs_protocol:$cvs_user\@$cvs_server:$cvs_root $cvs_command 2>&1"
 	ns_write "$cmd\n\n"
 	ns_log Notice "cvs-update: cmd=$cmd"
 
